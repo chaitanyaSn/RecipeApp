12874dd1345b8e8e80c2e24f3595d9fd747cf5b5
chaitanya <newarechaitanya832@gmail.com>
2024-09-11 16:05:22 +0530

    call api

--- src/Pages/FavoritesPage.jsx
+++ src/Pages/FavoritesPage.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import RecipeCard from '../components/RecipeCard';
-const fav=true;
-const FavoritesPage = () => {
+import React from "react";
+import RecipeCard from "../components/RecipeCard";
+import { getRandomColor } from "../lib/utils";
+
+const FavoritesPage = ({ recipe }) => {
+  const fav = JSON.parse(localStorage.getItem("favorites")) || [];
   return (
-    <div className='bg-[#faf9fb] flex-1 p-10 min-h-screen'>
-      <div className='max-w-screen-lg mx-auto'></div>
-      <p className='font-bold text-3xl md:text-5xl my-4'>Favourites Page</p>
-      {
-        !fav &&(
-          <div className='h-[80vh] flex flex-col items-center gap-4'>
+    <div className="bg-[#faf9fb] flex-1 p-10 min-h-screen">
+      <div className="max-w-screen-lg mx-auto">
+        <p className="font-bold text-3xl md:text-5xl my-4">Favourites Page</p>
+
+        {fav.length === 0 && (
+          <div className="h-[80vh] flex flex-col items-center gap-4">
             <img src="" alt="error" />
           </div>
-        )
-      }{
-        fav && (
-          <div className='grid grid-cols-1 md-grid-cols-2 lg:grid-cols-3 gap-4'>
-            <RecipeCard/>
-          </div>
-        )
-      }
-      
-    </div>
-  )
-}
+        )}
 
-export default FavoritesPage
+        <div className="grid grid-cols-1 md-grid-cols-2 lg:grid-cols-3 gap-4">
+          {fav.map((recipe) => (
+            <RecipeCard
+              key={recipe.label}
+              recipe={recipe}
+              {...getRandomColor()}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FavoritesPage;
--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,31 +1,66 @@
-import { Heart, HeartPulse, Soup } from 'lucide-react'
-import React from 'react'
+import { Heart, HeartPulse, Soup } from "lucide-react";
+import React, { useState, useEffect } from "react";
 
-const RecipeCard = () => {
+const RecipeCard = ({ recipe, bg, badge }) => {
+  const [isFav, setFav] = useState(localStorage.getItem("favorites")?.includes(recipe.label));
+
+
+  const addRecipeToFavourite = () => {
+    let favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const isRecipeAlreadyInFav = favorites.some((fav) => fav.label === recipe.label);
+    
+    if (isRecipeAlreadyInFav) {
+      favorites = favorites.filter((fav) => fav.label !== recipe.label);
+      setFav(false);
+    } else {
+      favorites.push(recipe);
+      setFav(true);
+    }
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  };
+
   return (
-    <div className='flex flex-col rounded-md bg-[#cde5a5] overflow-hidden p-3 relative'>
-    <a href="#" className='relative h-32'>
-      <img src="" alt="" className='rounded-md w-full object-cover cursor-pointer'/>
-      <div className='absolute bottom-2 left-2 rounded-full p-1 cursor-pointer bg-white  flex items-center gap-1 text-sm'><Soup/> 4 serving</div>
-      <div className='absolute top-1 right-2 rounded-full p-1 cursor-pointer bg-white'><Heart size={20} className='hover:fill-red-500 hover:text-red-500'/></div>
-    </a>
-    <div className='flex mt-1'>
-      <p className='fonr-bold tracking-wide'>Rosted chicken</p>
-    </div>
-    <p className='my-2'>Turkish Kitchen</p>
-    <div className='flex gap-2 mt-auto'>
-      <div className='flex gap-1 bg-[#d6f497]' items-center p-1 rounded-md>
-        <HeartPulse size={16}/>
-        <span className='text-sm font-semibold'>Gluten-free</span>
+    <div className={`flex flex-col rounded-md ${bg} overflow-hidden p-3 relative`}>
+      <a
+        href={`https://www.youtube.com/results?search_query=${recipe.label} recipe`}
+        target="_blank"
+        className="relative h-32"
+      >
+        <img
+          src={recipe.image}
+          alt={recipe.label}
+          className="rounded-md w-full h-full object-cover cursor-pointer"
+        />
+        <div className="absolute bottom-2 left-2 rounded-full p-1 cursor-pointer bg-white flex items-center gap-1 text-sm">
+          <Soup /> {recipe.yield} serving
+        </div>
+        <div
+          className="absolute top-1 right-2 rounded-full p-1 cursor-pointer bg-white"
+          onClick={(e) => {
+            e.preventDefault();
+            addRecipeToFavourite();
+          }}
+        >
+          {!isFav ? (
+            <Heart size={20} className="hover:fill-red-500 hover:text-red-500" />
+          ) : (
+            <Heart size={20} className="fill-red-500 text-red-500" />
+          )}
+        </div>
+      </a>
+      <div className="flex mt-1">
+        <p className="font-bold tracking-wide">{recipe.label}</p>
       </div>
-      <div className='flex gap-1 bg-[#d6f497]' items-center p-1 rounded-md>
-        <HeartPulse size={16}/>
-        <span className='text-sm font-semibold'>Gluten-free</span>
+      <p className="my-2">{recipe.cuisineType} Kitchen</p>
+      <div className="flex gap-2 mt-auto">
+        <div className={`flex gap-1 ${badge} items-center p-1 rounded-md`}>
+          <HeartPulse size={16} />
+          <span className="text-sm font-semibold">Gluten-free</span>
+        </div>
+        {/* Add more tags here if needed */}
       </div>
-     
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default RecipeCard
+export default RecipeCard;
